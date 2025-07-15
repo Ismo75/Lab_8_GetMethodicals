f8cbe06e9d3b7977002345cb7b73d89814094279
Ismo75 <ouare.ismo@outlook.com>
2025-07-15 06:10:44 -0400

    Lab completed

--- src/DevTest.java
+++ src/DevTest.java
@@ -3,7 +3,17 @@ import java.util.Scanner;
 public class DevTest {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        double value = SafeInput.getDouble(in, "Enter any double");
-        System.out.println("You entered: " + value);
+
+        // SSN Test
+        String ssn = SafeInput.getRegExString(in, "Enter SSN (format: ###-##-####)", "^\\d{3}-\\d{2}-\\d{4}$");
+        System.out.println("SSN Entered: " + ssn);
+
+        // M Number Test
+        String mNumber = SafeInput.getRegExString(in, "Enter M number (format: M12345)", "^(M|m)\\d{5}$");
+        System.out.println("M Number Entered: " + mNumber);
+
+        // Menu Choice Test
+        String menu = SafeInput.getRegExString(in, "Enter menu option [O/S/V/Q]", "^[OoSsVvQq]$");
+        System.out.println("Menu Choice Entered: " + menu);
     }
 }
